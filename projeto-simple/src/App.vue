02abc9ac8e31d4d93456ee0639d19e5481dc96ca
Fran Bernardo <francielebernardo88@hotmail.com>
2021-01-20 18:33:52 -0300

    instalação do vue, criação do projeto e plugins

---
+++ projeto-simple/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="app">
+  
+  {{ titulo }}
+  <button @click="titulo += '#' ">Alterar</button>
+  </div>
+  
+
+</template>
+
+<script>
+export default {
+  data: function(){
+    return{
+      titulo: 'teste usando vue js'
+    }
+  }
+}
+</script>
+
+<style>
+    #app{
+      color: red;
+    }
+</style>
