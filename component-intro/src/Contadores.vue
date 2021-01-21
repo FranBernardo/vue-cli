cc40258a4b3ff87b01520f8a79c4d407d2b0491e
Fran Bernardo <francielebernardo88@hotmail.com>
2021-01-21 18:56:26 -0300

    estudo vuejs

---
+++ component-intro/src/Contadores.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="contadores">
+        <app-contador v-for="c in 5 " :key="c" />
+    </div>
+</template>
+
+
+<script>
+import Contador from '../src/Contador'
+export default {
+    components: {
+        'app-contador': Contador
+    }
+    
+}
+</script>
